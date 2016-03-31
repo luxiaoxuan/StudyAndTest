a33ec0d8b863f7a705846993d8d2577846b4cea1
luxiaoxuan <luxiaoxuan1984@qq.com>
2016-03-31 17:12:52 +0800

    ..

--- WindowsFormsApplicationTrial/Form1.Designer.cs
+++ WindowsFormsApplicationTrial/Form1.Designer.cs
@@ -43,8 +43,8 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.ClientSize = new System.Drawing.Size(282, 255);
             this.Controls.Add(this.btnClickMe);
             this.Name = "Form1";
