31a18c731382346e4ae7ee67cb0202937b5a8ff3
dcs <dcs@dcs.dcs>
2016-04-01 10:59:46 +0800

    ..

--- WindowsFormsApplicationTrial/Form1.Designer.cs
+++ WindowsFormsApplicationTrial/Form1.Designer.cs
@@ -30,13 +30,15 @@
         {
             this.btnClickMe = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // btnClickMe
             // 
             this.btnClickMe.Location = new System.Drawing.Point(0, 0);
+            this.btnClickMe.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnClickMe.Name = "btnClickMe";
-            this.btnClickMe.Size = new System.Drawing.Size(480, 100);
+            this.btnClickMe.Size = new System.Drawing.Size(384, 80);
             this.btnClickMe.TabIndex = 0;
             this.btnClickMe.Text = "Smart Application";
             this.btnClickMe.UseVisualStyleBackColor = true;
@@ -45,20 +47,34 @@
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.textBox1.Location = new System.Drawing.Point(0, 100);
+            this.textBox1.Location = new System.Drawing.Point(0, 80);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.MaxLength = 45;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(480, 27);
+            this.textBox1.Size = new System.Drawing.Size(385, 23);
             this.textBox1.TabIndex = 1;
             this.textBox1.Text = "012345678901234567890123456789012345678901234";
             // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("MS UI Gothic", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(128)));
+            this.textBox2.Location = new System.Drawing.Point(390, 80);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox2.MaxLength = 40;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(360, 23);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "0123456789012345678901234567890123456789";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
-            this.ClientSize = new System.Drawing.Size(942, 155);
+            this.ClientSize = new System.Drawing.Size(754, 124);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.btnClickMe);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Smart Application";
             this.ResumeLayout(false);
@@ -70,6 +86,7 @@
 
         private System.Windows.Forms.Button btnClickMe;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
