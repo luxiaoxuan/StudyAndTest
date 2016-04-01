70c77d1f108df595d8056c4617d662e336ff8768
dcs <dcs@dcs.dcs>
2016-04-01 11:10:45 +0800

    ..

--- WindowsFormsApplicationTrial/Form1.Designer.cs
+++ WindowsFormsApplicationTrial/Form1.Designer.cs
@@ -31,6 +31,7 @@
             this.btnClickMe = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnClickMe
@@ -66,11 +67,23 @@
             this.textBox2.TabIndex = 2;
             this.textBox2.Text = "0123456789012345678901234567890123456789";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(388, 0);
+            this.button1.Margin = new System.Windows.Forms.Padding(0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(360, 80);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Smart Application";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.ClientSize = new System.Drawing.Size(754, 124);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.btnClickMe);
@@ -87,6 +100,7 @@
         private System.Windows.Forms.Button btnClickMe;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- WindowsFormsApplicationTrial/Form3.cs
+++ WindowsFormsApplicationTrial/Form3.cs
@@ -10,21 +10,11 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApplicationTrial
 {
-    public partial class Form1 : Form
+    public partial class Form3 : Form
     {
-        public Form1()
+        public Form3()
         {
             InitializeComponent();
-        }
-
-        private void btnClickMe_Click(object sender, EventArgs e)
-        {
-            new Form2().Show();
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            new Form3().Show();
         }
     }
 }
