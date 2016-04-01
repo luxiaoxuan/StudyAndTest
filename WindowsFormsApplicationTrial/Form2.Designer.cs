44a556499aa18d64c768656ee83247880b82b854
dcs <dcs@dcs.dcs>
2016-04-01 11:19:31 +0800

    ..

--- WindowsFormsApplicationTrial/Form2.Designer.cs
+++ WindowsFormsApplicationTrial/Form2.Designer.cs
@@ -28,21 +28,50 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnClickMe = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBox1.Location = new System.Drawing.Point(0, 82);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(0);
+            this.textBox1.MaxLength = 45;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(480, 23);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "012345678901234567890123456789012345678901234";
+            // 
+            // btnClickMe
+            // 
+            this.btnClickMe.Location = new System.Drawing.Point(0, 0);
+            this.btnClickMe.Margin = new System.Windows.Forms.Padding(0);
+            this.btnClickMe.Name = "btnClickMe";
+            this.btnClickMe.Size = new System.Drawing.Size(480, 80);
+            this.btnClickMe.TabIndex = 3;
+            this.btnClickMe.Text = "Smart Application";
+            this.btnClickMe.UseVisualStyleBackColor = true;
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(944, 162);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnClickMe);
             this.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form2";
             this.Text = "Form2";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnClickMe;
     }
 }
--- WindowsFormsApplicationTrial/Form3.Designer.cs
+++ WindowsFormsApplicationTrial/Form3.Designer.cs
@@ -28,18 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnClickMe = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // btnClickMe
+            // 
+            this.btnClickMe.Location = new System.Drawing.Point(0, 0);
+            this.btnClickMe.Margin = new System.Windows.Forms.Padding(0);
+            this.btnClickMe.Name = "btnClickMe";
+            this.btnClickMe.Size = new System.Drawing.Size(480, 80);
+            this.btnClickMe.TabIndex = 1;
+            this.btnClickMe.Text = "Smart Application";
+            this.btnClickMe.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBox1.Location = new System.Drawing.Point(0, 82);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(0);
+            this.textBox1.MaxLength = 45;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(480, 23);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "012345678901234567890123456789012345678901234";
             // 
             // Form3
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.ClientSize = new System.Drawing.Size(944, 162);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnClickMe);
             this.Name = "Form3";
             this.Text = "Form3";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnClickMe;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
