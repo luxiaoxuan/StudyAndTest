c2ceb2d0cf56faeec374440c7506550f3f04e77d
luxiaoxuan <luxiaoxuan1984@qq.com>
2016-04-01 10:48:30 +0800

    ..

--- WindowsFormsApplicationTrial/Form1.Designer.cs
+++ WindowsFormsApplicationTrial/Form1.Designer.cs
@@ -29,33 +29,47 @@
         private void InitializeComponent()
         {
             this.btnClickMe = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // btnClickMe
             // 
-            this.btnClickMe.Location = new System.Drawing.Point(85, 90);
+            this.btnClickMe.Location = new System.Drawing.Point(0, 0);
             this.btnClickMe.Name = "btnClickMe";
-            this.btnClickMe.Size = new System.Drawing.Size(120, 60);
+            this.btnClickMe.Size = new System.Drawing.Size(480, 100);
             this.btnClickMe.TabIndex = 0;
             this.btnClickMe.Text = "Smart Application";
             this.btnClickMe.UseVisualStyleBackColor = true;
             this.btnClickMe.Click += new System.EventHandler(this.btnClickMe_Click);
             // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBox1.Location = new System.Drawing.Point(0, 100);
+            this.textBox1.MaxLength = 45;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(480, 27);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "012345678901234567890123456789012345678901234";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
-            this.ClientSize = new System.Drawing.Size(282, 255);
+            this.ClientSize = new System.Drawing.Size(942, 155);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.btnClickMe);
             this.Name = "Form1";
             this.Text = "Smart Application";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btnClickMe;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
