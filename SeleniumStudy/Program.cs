d25822f28885c8c3906644a829d76d39edaeb2ef
luxiaoxuan <luxiaoxuan1984@qq.com>
2016-04-13 18:09:15 +0800

    Added form-input test website.

--- SeleniumStudy/Program.cs
+++ SeleniumStudy/Program.cs
@@ -1,4 +1,5 @@
-﻿using OpenQA.Selenium.Chrome;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,13 +17,29 @@ namespace SeleniumStudy
             var navi = wd.Navigate();
             navi.GoToUrl("http://www.yahoo.co.jp");
             var txt = wd.FindElementById("srchtxt");
-            txt.SendKeys("bilibili 中国");
-            var btn = wd.FindElementById("srchbtn");
+            txt.SendKeys("坂本ですが");
+            var a = wd.FindElementById("isearch");
+            a.Click();
+            //var btn = wd.FindElementById("srchbtn");
+            //btn.Click();
+
+            Thread.Sleep(3000);
+
+            a = wd.FindElementByClassName("pls");
+            a.Click();
+
+            txt = wd.FindElementById("f0dimp1");
+            txt.SendKeys("1920");
+            txt = wd.FindElementById("f0dimp2");
+            txt.SendKeys("1080");
+
+            var div = wd.FindElementByCssSelector("div.sb.cf");
+            var btn = div.FindElements(By.TagName("input")).First(e => e.GetAttribute("type") == "submit");
             btn.Click();
 
-            Thread.Sleep(5000);
+            Thread.Sleep(3000);
             
-            wd.Close();
+            wd.Quit();
         }
     }
 }
